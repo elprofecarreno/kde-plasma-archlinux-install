c3a54401e427876e11876e3d9dd04a690050fb0a
Cristian Carreño <elprofecarreno@gmail.com>
2026-07-27 22:22:32 -0400

    alpha install

--- desktop/install.sh
+++ desktop/install.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+#!/bin/bash
+
+echo "Habilitar multilib repository"
+# Habilitar multilib
+sed -i '/^\#\[multilib\]/,/^#Include/s/^#//' /etc/pacman.conf
+
+echo "Actualizar repositorios"
+# Actualizar repositorios
+pacman -Sy
+
+echo "Instalación de paquetes kde-plasma base"
+# Paquetes base
 pacman -S --noconfirm unzip \
 p7zip \
 unrar \
@@ -32,18 +44,71 @@ gst-plugins-good \
 gst-plugins-bad \
 gst-plugins-ugly \
 gst-libav \
-keepassxc \
-vulkan-icd-loader \
-vulkan-tools
+keepassxc
 
-sed -i 's/\#\[multilib\]/\[multilib\]' /etc/pacman.conf
-
-#linux-headers
-#nvidia
-
+echo "Instalación Teams -> Flathub"
 flatpak install -y flathub com.github.IsmaelMartinez.teams_for_linux
+echo "Instalación Chrome -> Flathub"
 flatpak install -y flathub com.google.Chrome
-
+echo "Instalación Discord -> Flathub"
 flatpak install -y flathub com.discordapp.Discord
+echo "Instalación Teams -> OnlyOffice"
 flatpak install -y flathub org.onlyoffice.desktopeditors
+
+PS3="Selecciona driver GPU: "
+
+select DRIVER in \
+    "Vulkan genérico" \
+    "NVIDIA propietario" \
+    "AMD Mesa" \
+    "Intel Mesa" \
+    "Sin driver"
+do
+    case $REPLY in
+        1)
+            pacman -S --noconfirm \
+                vulkan-icd-loader \
+                vulkan-tools
+            break
+            ;;
+
+        2)
+            pacman -S --noconfirm \
+                linux-headers \
+                nvidia \
+                nvidia-utils \
+                nvidia-settings \
+                lib32-nvidia-utils \
+                vulkan-icd-loader \
+                vulkan-tools
+            break
+            ;;
+
+        3)
+            pacman -S --noconfirm \
+                mesa \
+                vulkan-radeon \
+                lib32-vulkan-radeon \
+                vulkan-tools
+            break
+            ;;
+
+        4)
+            pacman -S --noconfirm \
+                mesa \
+                vulkan-intel \
+                lib32-vulkan-intel \
+                vulkan-tools
+            break
+            ;;
+
+        5)
+            break
+            ;;
+
+        *)
+            echo "Opción inválida"
+            ;;
+    esac
+done
 
